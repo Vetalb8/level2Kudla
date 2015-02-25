6c85459220d2c049f05512979700ef414a0eedaa
Vitalik <vitalya_kudla@mail.ru>
2015-02-25 20:06:28 +0500

    классы

---
+++ News/classes/AbstractModel.php
@@ -0,0 +1,53 @@
+<?php
+
+abstract class AbstractModel {
+    
+    protected static $table;
+    protected static $class;
+
+    public static function getAll()
+    {
+        $db = new DB;
+        $sql = 'SELECT * FROM ' . static::$table;
+        return $db->queryAll($sql, static::$class);
+    }
+
+    public static function getOne($id)
+    {
+        $db = new DB;
+        $sql = 'SELECT * FROM ' . static::$table . ' Where id =' . $id;
+        return $db->queryOne($sql, static::$class);
+    }
+
+    public static function addNew($title, $text)
+    {   
+        $db = new DB;
+        $sql = "INSERT INTO " . static::$table . " (title, text)
+                        VALUES ('$title', '$text')";
+    
+        return $db->queryNew($sql);
+    }
+
+    public static function updateNews($id, $title, $text)
+    {   
+        //Проверка
+        //if (!$id)
+            //return false;
+        //if ($title == '')
+            //return false;
+
+        $db = new DB;
+        $sql = "UPDATE " . static::$table . " SET title = '$title', text = '$text'
+                        WHERE id = '$id'";
+
+        return $db->queryUpdate($sql);
+    }
+
+    public static function IsPost(){
+        return $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
+
+    public static function IsGet(){
+        return $_SERVER['REQUEST_METHOD'] == 'GET';
+    }
+}
---
+++ News/classes/DB.php
@@ -0,0 +1,55 @@
+<?php
+
+class DB
+{
+    public function __construct()
+    {
+        mysql_connect('localhost', 'root', 'root');
+        mysql_select_db('news');
+        
+    }
+    
+    public function queryAll($sql, $class = 'stdClass' )
+    {
+        mysql_query("SET NAMES utf8");
+        $res = mysql_query($sql);
+        if (false === $res){
+            return  false;
+        }
+        $ret=[];
+        while($row = mysql_fetch_object($res, $class)){
+            $ret[] = $row;
+        }
+        
+        return $ret;
+    }
+    
+    public function queryOne($sql, $class = 'stdClass')
+    {
+        return $this->queryAll($sql, $class)[0];
+    }
+
+    public function queryNew($sql)
+    {
+        mysql_query("SET NAMES utf8");
+
+        $res = mysql_query($sql);
+
+        if (false === $res){
+            return  false;
+        }
+        return true;
+    }
+
+    public function queryUpdate($sql)
+    {
+        mysql_query("SET NAMES utf8");
+        //echo "$sql";die();
+        $res = mysql_query($sql);
+
+        if (false === $res){
+            return  false;
+        }
+        return true;
+    }
+}
